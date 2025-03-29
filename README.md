d8b667d157644d1d5253ee27428ac79d08aa8440
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-03-29 10:00:26 +0530

    added : basic layout and fiel based routing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # News App Using Next.js
+
+# Routing and page rendering:
+
+    File based routing, Component Types
+    1. Understanding Routing.
+    2. File name conventions and Project structure.
+    3. Server Components vs client components.
+
+1. icon.jpg in app folder will be used automatically for the fav icon of the website.
---
+++ app/news/[news_id]/page.js
@@ -0,0 +1,18 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsDetailPage({ params }) {
+  const newsId = params.news_id;
+
+  const newsItem = DUMMY_NEWS.find((item) => item.slug == newsId);
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
+}
