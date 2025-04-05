5bcf6e84f7ab00adf010b6ab4821d0bc8b454f27
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-04-06 00:46:41 +0530

    added : navigating programatically

--- app/news/[news_id]/@modal/(.)image/page.js
+++ app/news/[news_id]/@modal/(.)image/page.js
@@ -1,16 +1,20 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 const InterceptedImagePage = ({ params }) => {
   const newsItemSlug = params.news_id;
   const newsItem = DUMMY_NEWS.find((item) => item.slug == newsItemSlug);
+
+  // To navigate programatically
+  const router = useRouter();
 
   if (!newsItem) {
     notFound();
   }
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img
