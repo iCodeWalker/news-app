eed2d0bcd26dda0d85b020a6869fb8581e421b3b
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-04-06 22:59:08 +0530

    added : fetching data on server side

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,41 +1,54 @@
-"use client";
+// "use client";
 
 // import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/newsList";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  // #################### Code for client side data fetching #################
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+  // const [news, setNews] = useState();
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
+  // useEffect(() => {
+  //   async function fetchNews() {
+  //     setIsLoading(true);
 
-      const response = await fetch("http://localhost:8080/news");
+  //     const response = await fetch("http://localhost:8080/news");
 
-      if (!response.ok) {
-        setError("Failed to fetch news");
-        setIsLoading(false);
-      }
+  //     if (!response.ok) {
+  //       setError("Failed to fetch news");
+  //       setIsLoading(false);
+  //     }
 
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
+  //     const news = await response.json();
+  //     setIsLoading(false);
+  //     setNews(news);
+  //   }
 
-    fetchNews();
-  }, []);
+  //   fetchNews();
+  // }, []);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (error) {
+  //   return <p>{error}</p>;
+  // }
+
+  // ################################### Ends #####################################
+
+  // #################### Code for server side data fetching #################
+
+  // ## In server components we can directly send api request from the components
+
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
+  const news = await response.json();
   let newsContent;
 
   if (news) {
